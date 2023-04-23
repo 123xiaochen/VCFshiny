534009f78d65a356f8d558c61e94c7ec3ef951a1
123xiaochen <2218154118@qq.com>
2023-04-23 22:38:04 +0800

    2023-4-23-update

--- R/runApp.R
+++ R/runApp.R
@@ -9,5 +9,8 @@ startVCFshiny <- function(launch.browser = TRUE, port = getOption("shiny.port"),
 
   # set upload file size limit as 100MB
   options(shiny.maxRequestSize = 1000 * 1024^2, warn = -1, shiny.sanitize.errors = TRUE)
+
+  # addResourcePath(prefix = "Annovar", directoryPath = system.file("Annovar", ".", package = "VCFshiny"))
+
   shinyApp(ui = VCFui, server = VCFserver) %>% runApp(launch.browser = launch.browser, host = host, port = port)
 }
--- inst/shiny/modules/10.Annotation_ui.R
+++ inst/shiny/modules/10.Annotation_ui.R
@@ -1,81 +1,78 @@
 tabItem(
   tabName = "dataAnno",
   fluidRow(
-    box(
-      title = "Data Annotation", width = 3, status = "info", collapsible = T,
-      tabsetPanel(
-
-        tabPanel(
-          title = "Download datebase",
-          selectInput("Annovar_download_species", "Select Download Species Version:",
-                      c("hg38", "hg19"),
-                      selected = "hg38", width = "100%"),
-          selectInput("Annovar_download_web", "Select Download Datebase Webfrom:",
-                      choices = c("Annovar" = " -webfrom annovar ",
-                                  "UCSC" = " "),
-                      selected = "Annovar", width = "100%"),
-          textAreaInput("Annovar_download_name", "Input Download Datebase Name:", width = "100%",
-                        placeholder = "Please enter the name of the database! Ex: refGene"),
-          actionButton("Annovar_download_section", "Download Annovar Datebase", width = "100%",
-                       style = "background-color: #76a5af; border-radius: 28px;")
-        ),
-        tabPanel(
-          title = "Annotation",
-          fileInput(inputId = "input_file_anno",label = "Upload Your VCF Data:", multiple = F, accept = c("gz/zip")),
-          prettyRadioButtons(inputId = "annotation_fun",  label = "Select Annotation Function:",
-                             choices = c("Annovar", "VariantAnntation"),
-                             selected = "Annovar", inline = T,  animation = "tada", shape = "square", bigger = T, width = "100%"),
-          conditionalPanel("input.annotation_fun == 'Annovar'",
-            uiOutput("Annovar_anno_Species"),
-            uiOutput("Annovar_anno_Protocol"),
-            textAreaInput(inputId = "Annovar_opreation", label ="Select Annotation Operation:", width = "100%",
-                          placeholder = "Please enter the annotation mode in the order of the database! Ex: g,r,f (g,r is necessary)"),
+    tabsetPanel(
+      tabPanel(title = "Annovar Annotation",
+        box(title = "Annotation Parameter Setting", width = 3,
+            fileInput(inputId = "input_file_anno", label = "Upload Your VCF Data:", multiple = F, accept = c("gz/zip"), width = "100%"),
+            selectInput("Annovar_species", "Select Download Species Version:", c("hg38", "hg19"), selected = "hg38", width = "100%"),
+            selectInput(inputId = "Annovar_anno_type", label = "Select Annotation Type:",
+                        choices = c("Gene-based Annotation" = "-geneanno", "Region-based Annotation" = "-regionanno", "Filter-based Annotation" = "-filter"),
+                        selected = "Gene-based Annotation", multiple = T, width = "100%"),
+            uiOutput("Annovar_anno_gene_Database"),
+            uiOutput("Annovar_anno_region_Database"),
+            uiOutput("Annovar_anno_Filter_Database"),
             actionButton("Annovar_section", "Annovar Annotation Data", width = "100%",
-                         style = "background-color: #76a5af; border-radius: 28px;"),
-          ),
-          conditionalPanel("input.annotation_fun == 'VariantAnntation'",
-            uiOutput("VariantAnnotation_species"),
+                         style = "background-color: #76a5af; border-radius: 28px;")
+        ),
+        box(title = "Annotation Results Display", width = 9,
+            div(
+              style = "position: absolute; right:0.5em; top: 0.5em;",
+              downloadButton('Annovar_tab_download','Download TXT', class = "btn", icon = icon("download", lib = "glyphicon"))
+            ),
+            div(
+              uiOutput("Annovar_anno_id"),
+              shinycssloaders::withSpinner(DT::dataTableOutput("Annovar_anno_table"))
+            )
+        )
+      ),
+      tabPanel(title = "VariantAnnotation Annotation",
+        box(title = "Anntation Parameter Setting", width = 3,
+            fileInput(inputId = "input_file_Variantanno",label = "Upload Your VCF Data:", multiple = F, accept = c("gz/zip"), width = "100%"),
             selectInput("Variantanno_species", "organism type", c("hg38", "hg19"), selected = "hg38", width = "100%"),
             actionButton("VariantAnnotation_section", "VariantAnnotation Annotation Data", width = "100%",
-                          style = "background-color: #76a5af; border-radius: 28px;"),
-
-          )
+                         style = "background-color: #76a5af; border-radius: 28px;"),
+        ),
+        box(title = "Annotation Results Display", width = 9,
+            div(
+              style = "position: absolute; right:0.5em; top: 0.5em;",
+              downloadButton('variantanno_tab_download','Download TXT', class = "btn", icon = icon("download", lib = "glyphicon"))
+            ),
+            div(
+              uiOutput("VariantsAnno_anno_id"),
+              shinycssloaders::withSpinner(DT::dataTableOutput("variantanno_anno_table"))
+            )
+        )
+      ),
+      tabPanel(title = "Download Annovar Datebase",
+        uiOutput("Download_Annovar_Database_warning"),
+        box(title = "Download Parameter Setting", width = 3,
+            selectInput("Annovar_download_species", "Select Download Species Version:", c("hg38", "hg19"), selected = "hg38", width = "100%"),
+            selectInput("Annovar_download_database_type", "Select Download Database Type:", c("gene-based", "region-based", "filter-based"),
+                        selected = "gene-based", width = "100%"),
+            uiOutput("Annovar_Download_Name"),
+            actionButton("Annovar_download_section", "Download Annovar Datebase", width = "100%",
+                         style = "background-color: #76a5af; border-radius: 28px;"),
+        ),
+        box(title = "List of local datebase", width = 9,
+            div(
+              style = "position: absolute; right:0.5em; top: 0.5em;",
+              downloadButton('Annovar_Download_Tab_Download','Download TXT', class = "btn", icon = icon("download", lib = "glyphicon"))
+            ),
+            div(
+              DT::dataTableOutput("Annovar_Download_Tab")
+            )
+        ),
+        box(title = "List of Datebase avaliable", width = 12,
+            div(
+              style = "position: absolute; right:0.5em; top: 0.5em;",
+              downloadButton('Annovar_Database_Tab_Download','Download CSV', class = "btn", icon = icon("download", lib = "glyphicon"))
+            ),
+            div(
+              DT::dataTableOutput("Annovar_Database_Tab")
+            )
         )
       )
-    ),
-    box(title = "Data Display", width = 9, status = "primary", collapsible = T,
-        conditionalPanel(
-          "input.annotation_fun == 'Annovar'",
-          div(
-            style = "position: absolute; right:0.5em; top: 0.5em;",
-            downloadButton('Annovar_tab_download','Download TXT', class = "btn", icon = icon("download", lib = "glyphicon"))
-          ),
-          div(
-            shinycssloaders::withSpinner(DT::dataTableOutput("variantanno_datebase_filename")),
-            uiOutput("Annovar_anno_id"),
-            shinycssloaders::withSpinner(DT::dataTableOutput("Annovar_anno_table"))
-          )
-        ),
-        conditionalPanel(
-          "input.annotation_fun == 'VariantAnntation'",
-          div(
-            style = "position: absolute; right:0.5em; top: 0.5em;",
-            downloadButton('variantanno_tab_download','Download TXT', class = "btn", icon = icon("download", lib = "glyphicon"))
-          ),
-          div(
-            uiOutput("VariantsAnno_anno_id"),
-            shinycssloaders::withSpinner(DT::dataTableOutput("variantanno_anno_table"))
-          )
-        )
-    ),
-    box(title = "Annovar Datebase Display", width = 12,  status = "success", collapsible = T,
-        div(
-          style = "position: absolute; right:0.5em; top: 0.5em;",
-          downloadButton('Annovar_download_datebase_download','Download CSV', class = "btn", icon = icon("download", lib = "glyphicon"))
-        ),
-        div(
-          DT::dataTableOutput("Annovar_Datebase_Table")
-        )
     )
   )
 )
